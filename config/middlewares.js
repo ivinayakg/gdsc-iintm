3631ec5f0d42b7555a1c372c19b825410db77783
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2024-02-13 22:37:41 +0530

    fix env

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,24 @@
-module.exports = ({ env }) => [
-  "strapi::logger",
-  "strapi::errors",
-  "strapi::security",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: env("CORS_ORIGIN", ""),
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
+module.exports = ({ env }) => {
+  console.log(env("CORS_ORIGIN", "").split(","), "here");
+
+  return [
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::security",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+    {
+      name: "strapi::cors",
+      config: {
+        origin: env("CORS_ORIGIN", "").split(","),
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+        headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+        keepHeaderOnError: true,
+      },
     },
-  },
-];
+  ];
+};
